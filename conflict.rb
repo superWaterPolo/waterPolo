2ec3862498ee9c6bdc336cfbfdfa15f9e718fbda
DisasterNull <tomoya.17843@gmail.com>
2023-06-09 15:40:26 +0900

    衝突判定

--- conflict.rb
+++ conflict.rb
@@ -1,6 +1,8 @@
 
 def conflict(a_x, a_y, b_x, b_y)
-    if a_x == b_x && a_y == b_y
-        return true
+    if (a_x-b_x).abs <= 32
+        if (a_y-b_y).abs <= 32
+            return 0
+        end
     end
 end
