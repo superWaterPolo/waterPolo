e56d69e59d6f2b385a2e466b75badb5262e78f80
sakumarinji <s206028@matsu.shimane-u.ac.jp>
2023-06-16 15:59:49 +0900

    'score,item'

--- main.rb
+++ main.rb
@@ -4,6 +4,10 @@ require_relative 'draw'
 require_relative 'gameOver'
 require_relative 'ghostMove'
 require_relative 'pacmanMove'
+require_relative 'item'
+ 
+font = Font.new(32)
+count = 1
 
 Window.width = 640
 Window.height = 480
@@ -57,7 +61,14 @@ Window.loop do
       end
     end
   end
+  #item
+  pacman_speed = item(pacman_x,pacman_y,pacman_speed)
+
+
   # 描画処理
   draw(pacman_x, pacman_y, C_YELLOW)
   draw(ghost_x, ghost_y, C_RED)
+
+  Window.draw_font(32,32,"score:#{count}",font)
+  count=count+1  
 end
